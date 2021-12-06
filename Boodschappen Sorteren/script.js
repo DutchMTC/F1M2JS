8adf810a205e5895a2a57b7c2251857eb280bbd5
Miyaki Truong <32574@ma-web.nl>
2021-12-06 10:43:18 +0100

    Les 3

--- Boodschappen Sorteren/script.js
+++ Boodschappen Sorteren/script.js
@@ -19,3 +19,13 @@ function zetInGroenteLa(groente){
     groentevak.appendChild(nieuwElement);
 }
 
+var colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+var i = 1;
+
+window.setInterval(function(){
+    document.body.style.backgroundColor = colors[i];
+    i++;
+    if (i === colors.length){
+        i=0;
+    }
+}, 5000);
